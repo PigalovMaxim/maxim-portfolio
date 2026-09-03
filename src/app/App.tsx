3507f14031226df1b59063069f01239c012e5da4
PigalovMaxim <PigalovMaximS@yandex.ru>
2026-09-03 21:24:46 +0300

    Init

---
+++ src/app/App.tsx
@@ -0,0 +1,10 @@
+import { HomePage } from '../pages/home'
+import { StoreProvider } from './providers/StoreProvider'
+
+export function App() {
+  return (
+    <StoreProvider>
+      <HomePage />
+    </StoreProvider>
+  )
+}
---
+++ src/pages/home/ui/HomePage.tsx
@@ -0,0 +1,11 @@
+import styles from './HomePage.module.scss'
+
+export function HomePage() {
+  return (
+    <main className={styles.page}>
+      <h1 className={styles.title}>
+        Maxim<span>-portfolio</span>
+      </h1>
+    </main>
+  )
+}
